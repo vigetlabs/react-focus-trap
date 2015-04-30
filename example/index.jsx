e049c5cff13a7020835018ce2b992a8f4a534976
Nate Hunzaker <nate.hunzaker@gmail.com>
2015-04-30 16:42:24 -0400

    [update] Clean up example

--- example/index.jsx
+++ example/index.jsx
@@ -14,22 +14,16 @@ var Component = React.createClass({
     return (
       <div>
         <button onClick={ this._onOpenOne}>Open Dialog One</button>
-
         <FocusTrap onExit={ this._onExitOne } active={ this.state.focusOne }>
           <h1>This is the primary dialog</h1>
-          <p><a href="http://google.com" >Focus one</a></p>
+          <p><a href="http://google.com">Focus one</a></p>
           <p><a href="http://google.com">Focus two</a></p>
-          <p><a href="http://google.com">Focus three</a></p>
-          <p><a href="http://google.com">Focus four</a></p>
-          <button onClick={ this._onOpenTwo}>Open Dialog Two</button>
+          <button onClick={ this._onOpenTwo }>Open Dialog Two</button>
         </FocusTrap>
-
         <FocusTrap onExit={ this._onExitTwo } active={ this.state.focusTwo }>
           <h1>This is the secondary dialog</h1>
-          <p><a href="http://google.com" >Focus one</a></p>
+          <p><a href="http://google.com">Focus one</a></p>
           <p><a href="http://google.com">Focus two</a></p>
-          <p><a href="http://google.com">Focus three</a></p>
-          <p><a href="http://google.com">Focus four</a></p>
         </FocusTrap>
       </div>
     );
