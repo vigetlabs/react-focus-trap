c3954dca349c0a232c242acf45adf1c859f5e2d8
Nathan Hunzaker <nate.hunzaker@gmail.com>
2017-05-05 17:06:46 -0400

    Use React Classes to avoid Deprecations

--- src/focal-point.js
+++ src/focal-point.js
@@ -15,28 +15,25 @@ if (typeof document !== 'undefined' && document.activeElement) {
   stack.push(document.activeElement)
 }
 
-let FocalPoint = React.createClass({
-
-  getDefaultProps() {
-    return {
-      element : 'div'
-    }
-  },
+class FocalPoint extends React.Component {
+  static defaultProps = {
+    element: 'div'
+  }
 
   contains(element) {
     return this.refs.root.contains(element)
-  },
+  }
 
   focus() {
     if (this.contains(document.activeElement) === false) {
       this.refs.root.focus()
     }
-  },
+  }
 
   trapFocus(e) {
     clearTimeout(timer)
     timer = setTimeout(_ => stack[stack.length - 1].focus(), 10)
-  },
+  }
 
   returnFocus() {
     let { anchor } = this.state
@@ -44,16 +41,18 @@ let FocalPoint = React.createClass({
     // When transitioning between pages using hash route state,
     // this anchor is some times lost. Do not attempt to focus
     // on a non-existent anchor.
-    if (anchor && typeof anchor === 'object' && typeof anchor.focus === 'function') {
+    if (
+      anchor && typeof anchor === 'object' && typeof anchor.focus === 'function'
+    ) {
       anchor.focus()
     }
-  },
+  }
 
   componentWillMount() {
     if (typeof document !== 'undefined') {
       this.setState({ anchor: document.activeElement })
     }
-  },
+  }
 
   componentDidMount() {
     stack.push(this)
@@ -61,7 +60,7 @@ let FocalPoint = React.createClass({
     this.trapFocus()
 
     document.addEventListener('focus', this._onBlur, true)
-  },
+  }
 
   componentWillUnmount() {
     stack = stack.filter(i => i !== this)
@@ -71,19 +70,19 @@ let FocalPoint = React.createClass({
     clearTimeout(timer)
 
     this.returnFocus()
-  },
+  }
 
   render() {
-    let { children, element:Element, className } = this.props
+    let { children, element: Element, className } = this.props
 
     return (
-      <Element ref="root" tabIndex="0" className={ className }>
-        { children }
+      <Element ref="root" tabIndex="0" className={className}>
+        {children}
       </Element>
     )
-  },
+  }
 
-  _onBlur(event) {
+  _onBlur = event => {
     let current = stack[stack.length - 1]
 
     if (current && current.contains(event.target) === false) {
@@ -91,6 +90,6 @@ let FocalPoint = React.createClass({
       this.trapFocus()
     }
   }
-})
+}
 
 module.exports = FocalPoint
---
+++ src/focus-trap.js
@@ -0,0 +1,36 @@
+let FocalPoint = require('./focal-point')
+let React = require('react')
+
+class FocusTrap extends React.Component {
+  static defaultProps = {
+    active: true,
+    className: 'focus-trap'
+  }
+
+  render() {
+    let { active, className, children, element, onExit } = this.props
+
+    if (!active) return null
+
+    return (
+      <div className={`${className}-wrapper`} onKeyUp={this._onKeyUp}>
+        <div
+          aria-hidden="true"
+          className={`${className}-backdrop`}
+          onClick={onExit}
+        />
+        <FocalPoint ref="focus" className={className} element={element}>
+          {children}
+        </FocalPoint>
+      </div>
+    )
+  }
+
+  _onKeyUp = event => {
+    if (event.key === 'Escape' && 'onExit' in this.props) {
+      this.props.onExit()
+    }
+  }
+}
+
+module.exports = FocusTrap
